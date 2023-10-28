b56209b922048ad38aaf4fc4e89a0986b75c36b8
Koisu <127571802+Koisu-unavailable@users.noreply.github.com>
2023-10-28 14:14:30 -0600

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,11 @@
+class Cursor():
+    def __init__(self):
+        led.plot(2, 4)
+class OBJ():
+    def __init__(self):
+        led.
+        led.plot(0, 0)
+
+
+Cursor()
+OBJ()
---
+++ main.ts
@@ -0,0 +1,17 @@
+class Cursor {
+    constructor() {
+        led.plot(2, 4)
+    }
+    
+}
+
+class OBJ {
+    constructor() {
+        led
+        led.plot(0, 0)
+    }
+    
+}
+
+new Cursor()
+new OBJ()
